18d4f280b88761719b73aa266f9e8a2eed0b662a
dmitryspeliy <dmitriypospelov93@gmail.com>
2022-09-16 21:12:04 +0700

    Commit

---
+++ calculator/src/main/java/pro/sky/calculator/Controller.java
@@ -0,0 +1,41 @@
+package pro.sky.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class Controller {
+
+    @Autowired
+    ServiceCal serviceCal;
+
+    @GetMapping()
+    public String hello() {
+        return serviceCal.sayHello();
+    }
+
+    @GetMapping("/plus")
+    public String sum(@RequestParam(value = "num1") String num1, @RequestParam(value = "num2") String num2) {
+        return serviceCal.sum(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String subtract(@RequestParam(value = "num1") String num1, @RequestParam(value = "num2") String num2) {
+        return serviceCal.subtract(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam(value = "num1") String num1, @RequestParam(value = "num2") String num2) {
+        return serviceCal.multiply(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String division(@RequestParam(value = "num1") String num1, @RequestParam(value = "num2") String num2) {
+        return serviceCal.division(num1, num2);
+    }
+
+}
---
+++ calculator/src/main/java/pro/sky/calculator/ToDoCalculator.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator;
+
+public interface ToDoCalculator {
+    String sayHello();
+
+    String sum(String a, String b);
+
+    String division(String a, String b);
+
+    String subtract(String a, String b);
+
+    String multiply(String a, String b);
+}
